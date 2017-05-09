b6d8bf6e05ec676b1b3897f6ce44c38743af72de
wangben0122 <1124979627@qq.com>
2017-05-09 15:19:57 +0800

    Initial commit

---
+++ app/src/main/java/test/jiyun/com/kaiyuanzhongguo/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package test.jiyun.com.kaiyuanzhongguo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+/**
+ * Created by admin on 2017/5/9.
+ */
+
+public class WelcomeActivity extends Activity {
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
